0b7e9fe52851c0741b10221ece024bd1f90bfb34
gianfranco <gmanieli5647@gmail.com>
2024-05-29 14:15:14 +1000

    work
    did some work

---
+++ led_oop/led_oop.ino
@@ -0,0 +1,56 @@
+#define LED_PIN 13
+
+class Led 
+{
+private:
+  byte pin;
+public:
+  Led() {} // do not use!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+  Led(byte pin)
+  {
+    this->pin = pin;
+  }
+
+  void init()
+  {
+    pinMode(pin, OUTPUT);
+    Serial.begin(9600);
+    Serial.println("test");
+  }
+
+  void init(byte defaultState)
+  {
+    init();
+    if (defaultState == HIGH) {
+      on();
+    }
+    else {
+      off();
+    }
+  }
+
+  void on()
+  {
+    digitalWrite(pin, HIGH);
+  }
+
+  void off()
+  {
+    digitalWrite(pin, LOW);
+  }
+};
+
+Led led(LED_PIN);
+
+
+void setup() {
+  led.init();
+}
+
+void loop() {
+  led.on();
+  delay(500);
+  led.off();
+  delay(500);
+} 
